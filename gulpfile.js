79f886577e5b1efabcdcbcba94f5da0365e79609
junshen <redwolf85911@gmail.com>
2016-12-18 16:16:12 +0800

    big changes

--- gulpfile.js
+++ gulpfile.js
@@ -6,28 +6,30 @@ var cssnano = require('gulp-cssnano');
 var run = require('run-sequence');
 
 gulp.task('default', function (cb) {
-  run ('clean:dist', 'html', 'image', 'copy')
+    run('clean:dist', 'html', 'image', 'copy')
 })
 
 // clean
 var del = require('del');
-gulp.task('clean:dist', function() {
-  return del.sync('dist');
+gulp.task('clean:dist', function () {
+    return del.sync('dist');
 })
 
 // html related task
 var inlinehtmlimg = require('gulp-inline-image-html');
 var htmlmin = require('gulp-htmlmin');
-gulp.task('html', function(){
-  return gulp.src('./*.html')
-    .pipe(useref())
-    // Minifies only if it's a JavaScript file
-    .pipe(gulpIf('*.js', uglify()))
-    // Minifies only if it's a CSS file
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulpIf('*.html', htmlmin({collapseWhitespace: true})))
-    // .pipe(gulpIf('*.html', inlinehtmlimg()))
-    .pipe(gulp.dest('dist'))
+gulp.task('html', function () {
+    return gulp.src('./*.html')
+        .pipe(useref())
+        // Minifies only if it's a JavaScript file
+        .pipe(gulpIf('*.js', uglify()))
+        // Minifies only if it's a CSS file
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulpIf('*.html', htmlmin({
+            collapseWhitespace: true
+        })))
+        // .pipe(gulpIf('*.html', inlinehtmlimg()))
+        .pipe(gulp.dest('dist'))
 });
 
 
@@ -37,35 +39,49 @@ var imageResize = require('gulp-image-resize');
 var imagemin = require('gulp-imagemin');
 
 gulp.task('image', function () {
-  // generate small and medium images
-  var smallImages = gulp.src('images/**/*.+(png|jpg|gif|svg)')
-    .pipe(imageResize({
-      width: '40%',
-      height: '40%',
-      imageMagick: true
-    }))
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/images/small'))
 
-  var mediumImages = gulp.src('images/**/*.+(png|jpg|gif|svg)')
-    .pipe(imageResize({
-      width: '80%',
-      height: '80%',
-      imageMagick: true
-    }))
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/images/medium'))
 
-  // minimize all the image size
-  var originalImages = gulp.src('images/**/*.+(png|jpg|gif|svg)')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/images'))
+    // generate small and medium images
+    var smallImages = gulp.src('images/**/*.+(png|jpg|gif|svg|jpeg)')
+        .pipe(gulpIf('hupai.jpg', imageResize({
+            width: '30%',
+            height: '30%'
+        }), imageResize({
+            width: '40%',
+            height: '40%'
+        })))
+        .pipe(gulpIf('*.jpeg', imageResize({
+            format: 'jpg'
+        })))
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images/small'))
 
-  return merge(smallImages, mediumImages, originalImages)
+    var mediumImages = gulp.src('images/**/*.+(png|jpg|gif|svg|jpeg)')
+        .pipe(imageResize({
+            width: '80%',
+            height: '80%'
+        }))
+        .pipe(gulpIf('*.jpeg', imageResize({
+            format: 'jpg'
+        })))
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images/medium'))
+
+    // minimize all the image size
+    var originalImages = gulp.src('images/**/*.+(png|jpg|gif|svg|jpeg)')
+        .pipe(gulpIf('*.jpeg', imageResize({
+            format: 'jpg'
+        })))
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+
+    var combineAll = merge(smallImages, mediumImages, originalImages)
+
+    return combineAll
 })
 
 // copy rest files to dest
-gulp.task('copy', function() {
-  return gulp.src('assets/**/*')
-    .pipe(gulp.dest('dist/assets'))
+gulp.task('copy', function () {
+    return gulp.src('assets/**/*')
+        .pipe(gulp.dest('dist/assets'))
 })
